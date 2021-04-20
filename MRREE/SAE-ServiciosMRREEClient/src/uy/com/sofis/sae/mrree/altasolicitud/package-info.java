b037e176773cc15b4cafb1e787ff6736103bd163
sebastianramirezarrieta <sebastian.ramirez@agesic.gub.uy>
2021-04-20 01:01:32 -0300

    Revert "Add files via upload"
    This reverts commit fe7bf3b2a1b8bc4cdf406f5f1571912c450afd28.

--- MRREE/SAE-ServiciosMRREEClient/src/uy/com/sofis/sae/mrree/altasolicitud/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "TramitesExterior", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package uy.com.sofis.sae.mrree.altasolicitud;
